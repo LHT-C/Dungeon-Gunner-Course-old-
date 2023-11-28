ba468bc21d7acd9f7988e3be8c7f97299fa73cd4
LHT-C <124505887+LHT-C@users.noreply.github.com>
2023-11-29 00:03:12 +0800

    2023.11.28
    11-3. Open, Lock, And Unlock Doors

--- Assets/Scripts/Misc/Settings.cs
+++ Assets/Scripts/Misc/Settings.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public static class Settings
 {
+    #region UNITS
+    public const float pixelsPerUnit = 16f;
+    public const float tileSizePixels = 16f;
+    #endregion
+
     #region DUNGEON BUILD SETTINGS
     public const int maxDungeonRebuildAttemptsForRoomGraph = 1000;//尝试重建节点的最大次数
     public const int maxDungeonBuildAttempts = 10;//尝试建造地牢的最大次数
@@ -29,5 +34,15 @@ public static class Settings
     public static int rollRight = Animator.StringToHash("rollRight");
     public static int rollLeft = Animator.StringToHash("rollLeft");
     public static int rollDown = Animator.StringToHash("rollDown");
+
+    // Animator parameters - Door
+    public static int open = Animator.StringToHash("open");
+
     #endregion
+
+    #region GAMEOBJECT TAGS
+    public const string playerTag = "Player";
+    public const string playerWeapon = "playerWeapon";
+    #endregion
+
 }
